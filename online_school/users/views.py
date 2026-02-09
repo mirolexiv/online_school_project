42ababb7523118e128099665f8ec6d14c37a36aa
Oleksandr Miroshnychenko <mirolexiv@gmail.com>
2026-02-09 23:36:49 +0200

    Added usurs login and logout

--- online_school/users/views.py
+++ online_school/users/views.py
@@ -1,13 +1,27 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+
+from django.urls import reverse
 
 from users.forms import LoginForm
 
 
 def login_user(request):
-    form = LoginForm()
-    return render(request, 'users/login.html', {'form': form})
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user and user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/lessons/lessons/')
+    else:
+        form = LoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 
 def logout_user(request):
-    return HttpResponse("logout")
+    logout(request)
+    return HttpResponseRedirect(reverse('users:login'))
